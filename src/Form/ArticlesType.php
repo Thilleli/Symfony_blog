d5418b0e5eb272e29cd8671eeedc54226589f755
Thilleli <lilybelhocine006@gmail.com>
2023-06-06 22:56:11 +0200

    create - update auteur

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Titre', TextType::class)
+            ->add('Contenu', TextareaType::class)
+            ->add('Slug', TextType::class, ['required' => false])
+            ->add('Date_de_publication', DateTime::class, ['required' => false])
+            ->add('Auteur', EntityType::class, [
+                'class' => 'App\Entity\Auteur',
+                'choice_label' => 'Nom',
+            ])
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
